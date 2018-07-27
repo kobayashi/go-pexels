37ec38ed84585bd9b25d17cd7fc0af923308397f
kobayashi <abok.1k@gmail.com>
2018-07-26 23:20:29 -0400

    modified:   pexels.go

--- pexels.go
+++ pexels.go
@@ -1,87 +1,41 @@
 package pexels
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
-	"net/url"
+
+	"github.com/dghubble/sling"
 )
 
-const (
-	baseURL = "https://api.pexels.com/v1/search/"
-)
+const baseURL = "https://api.pexels.com/"
 
-// Option is providing APIKEY
-type Option struct {
-	APIKey string
+type Service struct {
+	sling *sling.Sling
 }
 
-// Queries is Query options
-type Queries struct {
-	Query   []string
-	PerPage int
-	Page    int
+type PexelsError struct {
+	APIError string `json:"error"`
 }
 
-// Client creats http client with Option
-type Client struct {
-	Option *Option
-	Client *http.Client
+type SearchParams struct {
+	Query   string `url:"query,omitempty"`
+	PerPage int    `url:"per_page,omitempty"`
+	Page    int    `url:"page,omitempty"`
 }
 
-// SearchPhotos is to find photos with expected queries
-func (c *Client) SearchPhotos(queries Queries) (photos *Results, err error) {
-	params := map[string]interface{
-		"queries": queries,
-	}
-
-	_, err = c.makeRequest(params, &potos)
-	if err != nil {
-		panic(err)
-	}
-	return photos, nil
+type TrendSearchParams struct {
+	PerPage int `url:"per_page,omitempty"`
+	Page    int `url:"page,omitempty"`
 }
 
-func (c *Client) makeRequest(params map[string]interface{}, p interface{}) (statusCode int, err error) {
-	queryURL, err := url.Parse(baseURL)
-	if err != nil {
-		return 0, err
+// NewCLient returns a new Client
+func NewClient(httpClient *http.Client, apikey string) *Service {
+	return &Service{
+		sling: sling.New().Client(httpClient).Base(baseURL).Set("Authorization", apikey),
 	}
-
-	req, err := http.NewRequest("GET", fmt.Sprint(queryURL), nil)
-	req.Header.Add("Authorization", c.Option.APIKey)
-	q := req.baseURL.Query()
-	for k, v := range params {
-		q.Add(k, v)
-	}
-
-	response, err := c.Client.Do(req)
-	if err != nil {
-		if response != nil {
-			return response.StatusCode, err
-		} else {
-			return 500, err
-		}
-	}
-
-	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		return response.StatusCode, errors.New(response.Status)
-	}
-
-	err = json.NewDecoder(response.Body).Decode(&p)
-	return response.StatusCode, err
 }
 
-func New(option *Option, httpClient *http.Client) *Client {
-	if httpClient == nil {
-		httpClient = http.DefaultClient
-	}
-
-	return &Client{
-		Option: option,
-		Client: httpClient,
-	}
+// Error returns api error
+func (e PexelsError) Error() string {
+	return fmt.Sprintf("pexles: %s", e.APIError)
 }
